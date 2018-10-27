8c766699a524d120dab33f44091b1b0f735e86cc
unknown <serranotom@gmail.com>
2018-10-27 00:03:17 -0400

    #11 Initialized the UI elements for the set threshold activity. Added a back button to go back to main activity.

--- FrontEnd/app/src/main/java/com/example/gamingpc/easyplants/setThresholdActivity.java
+++ FrontEnd/app/src/main/java/com/example/gamingpc/easyplants/setThresholdActivity.java
@@ -1,13 +1,50 @@
 package com.example.gamingpc.easyplants;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class setThresholdActivity extends AppCompatActivity {
+
+    private static final String TAG = "setThresholdActivity";
+
+    // Set up UI elements
+    Button save;
+    EditText lowerThresh;   // User input of lower humidity bound
+    EditText upperThresh;   // User input of upper humidity bound
+    TextView currentMinMax; // Displays the current set threshold
+
+    // Initializes the UI elements
+    private void setup() {
+        // The text fields
+        lowerThresh = findViewById(R.id.text_newMin);
+        upperThresh = findViewById(R.id.text_newMax);
+        currentMinMax = findViewById(R.id.text_currentMinMax);
+        // Save button and its listener
+        save = findViewById(R.id.button_save);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO save new threshold values
+                startActivity(new Intent(setThresholdActivity.this, MainActivity.class));
+            }
+        });
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_threshold);
+
+        // Allow the action bar back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setup();
     }
 }
