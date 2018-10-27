e4ec9e9a525ab19c33259ec11acabbecf93ec208
unknown <serranotom@gmail.com>
2018-10-26 23:00:25 -0400

    #11 Created a Set Threshold dialog and linked it to the main activity via a temporary button

--- FrontEnd/app/src/main/java/com/example/gamingpc/easyplants/MainActivity.java
+++ FrontEnd/app/src/main/java/com/example/gamingpc/easyplants/MainActivity.java
@@ -2,14 +2,42 @@ package com.example.gamingpc.easyplants;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Main Activity";
 
+    // Set up the UI elements
+    Button setThreshold;
+    Button setSchedule;
+    TextView humidityDisplay;
+
+    // Initializes the UI elements for the main activity
+    private void setup() {
+
+        // Initialize the set Threshold button and allow it to open the dialog
+        setThreshold = findViewById(R.id.button_thresh);
+        setThreshold.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: Opening setThresholdDialog");
+
+                setThresholdDialog dialog = new setThresholdDialog();
+                dialog.show(getSupportFragmentManager(), "AddCourseDialogue");
+            }
+        });
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setup();
     }
 }
