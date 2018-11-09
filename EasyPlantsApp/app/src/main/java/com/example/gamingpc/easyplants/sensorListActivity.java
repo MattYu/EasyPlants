335965531e4beb0a18dd976e203301302e002db1
unknown <serranotom@gmail.com>
2018-11-09 17:34:31 -0500

    Created sensorListActivity and editing the mainActivity in line with agreed upon UI changes

---
+++ EasyPlantsApp/app/src/main/java/com/example/gamingpc/easyplants/sensorListActivity.java
@@ -0,0 +1,28 @@
+package com.example.gamingpc.easyplants;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class sensorListActivity extends AppCompatActivity {
+
+    private static final String TAG = "sensorListActivity";
+
+    // UI elements
+    private ListView sensorList;
+
+    // Called to set up all the UI elements
+    private void setup() {
+        
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor_list);
+
+        // Enable the back button to the mainActivity
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setup();
+    }
+}
