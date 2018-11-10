0116ee5477d6d2566510a10d14261eeb2991e52e
MohamedElgak <Mohamed.aljack@live.com>
2018-11-09 21:15:30 -0500

    #38 Send json to firebase
    Sending json to firebase

--- Arduino/plant_wifi/plant_wifi.ino
+++ Arduino/plant_wifi/plant_wifi.ino
@@ -1,45 +1,102 @@
-#include <UnoWiFiDevEd.h>
+#include <ArduinoJson.h>
 
 
 
-#define CONNECTOR "mqtt"
-#define TOPIC "/arduino/analog/A1"
-//#define SERVER_ADDR   "192.168.0.103" // change ip address with your server ip address
+// FirebaseDemo_ESP8266 is a sample that demo the different functions
+// of the FirebaseArduino API.
 
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
 
-  int sensor_pin = A0;
-  int output_value ;
-  int soilPower = 7;//Variable for Soil moisture Power
+// Set these to run example.
+#define FIREBASE_HOST "easyplants-5930b.firebaseio.com"
+#define FIREBASE_AUTH "UW5P18z1BeVE3qli40oO4VjQtmjz0ja2jPI2VYT2"
+#define WIFI_SSID "wifi_plant"
+#define WIFI_PASSWORD "AwesomePlants"
 
 void setup() {
+  Serial.begin(115200);
 
-  Serial.begin(9600);
-  Serial.println("Reading From the Sensor ...");
-  pinMode(soilPower, OUTPUT);//Set D7 as an OUTPUT
-  digitalWrite(soilPower, LOW);//Set to LOW so no power is flowing through the sensor
-  Ciao.begin();
+  // connect to wifi.
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+  
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
 
+int n = 0;
 
-void loop(){
-    digitalWrite(soilPower, HIGH);//turn D7 "On"
-    delay(10);//wait 10 milliseconds  
-   output_value = analogRead(sensor_pin);//Read the SIG value form sensor 
-    digitalWrite(soilPower, LOW);//turn D7 "Off"
-   // output_value= map(output_value, 0, 890, 0, 100);
+void loop() {
+  // set value
+  Firebase.setFloat("number", 42.0);
+  // handle error
+  if (Firebase.failed()) {
+      Serial.print("setting /number failed:");
+      Serial.println(Firebase.error());  
+      return;
+  }
+  delay(1000);
+  
+  // update value
+  Firebase.setFloat("number", 43.0);
+  // handle error
+  if (Firebase.failed()) {
+      Serial.print("setting /number failed:");
+      Serial.println(Firebase.error());  
+      return;
+  }
+  delay(1000);
 
-   Serial.print("Moisture : ");
+  // get value 
+  Serial.print("number: ");
+  Serial.println(Firebase.getFloat("number"));
+  delay(1000);
 
-   Serial.print(output_value);
+  // remove value
+  Firebase.remove("number");
+  delay(1000);
 
-   Serial.println("%");
+  // set string value
+  Firebase.setString("message", "hello world");
+  // handle error
+  if (Firebase.failed()) {
+      Serial.print("setting /message failed:");
+      Serial.println(Firebase.error());  
+      return;
+  }
+  delay(1000);
+  
+  // set bool value
+  Firebase.setBool("truth", false);
+  // handle error
+  if (Firebase.failed()) {
+      Serial.print("setting /truth failed:");
+      Serial.println(Firebase.error());  
+      return;
+  }
+  delay(1000);
+  StaticJsonBuffer<200> jsonBuffer;
+JsonObject& root = jsonBuffer.createObject();
+  root["sensor_id"] = 1;
+  root["humidity_value"] = 7;
+  root["sensor_security_code"] = 1;
 
-
-   delay(5000);
-   int data_value = analogRead(sensor_pin); // data value from analog pin 1
-   Ciao.print("Moisture :");
-   Ciao.print(String(output_value));
-   Ciao.println("");
-   Ciao.write(CONNECTOR, TOPIC, String(data_value)); // pushes data into a channel called PIN_A1
-
+  // append a new value to /logs
+  String name = Firebase.push("message_list",root);
+  // handle error
+  if (Firebase.failed()) {
+      Serial.print("pushing /logs failed:");
+      Serial.println(Firebase.error());  
+      return;
+  }
+  Serial.print("pushed: /logs/");
+  Serial.println(name);
+  delay(1000);
 }
