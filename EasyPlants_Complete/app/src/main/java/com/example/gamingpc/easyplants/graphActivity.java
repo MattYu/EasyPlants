cc317d7dc05d8aa061da459ec11b3249193cc28a
Unknown <jonathan.cournoyer@scigilian.com>
2018-11-25 17:56:53 -0500

    #37 - Adding Library for Graphing
    - Added Hellocharts library
    - Forced Activities to Portrait or Landscape modes

--- EasyPlants_Complete/app/src/main/java/com/example/gamingpc/easyplants/graphActivity.java
+++ EasyPlants_Complete/app/src/main/java/com/example/gamingpc/easyplants/graphActivity.java
@@ -1,9 +1,13 @@
 package com.example.gamingpc.easyplants;
 
+import android.app.Fragment;
 import android.content.Intent;
+import android.hardware.Sensor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import com.example.gamingpc.easyplants.Models.SensorData;
 import com.google.firebase.auth.FirebaseAuth;
@@ -16,24 +20,41 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import lecho.lib.hellocharts.view.LineChartView;
+
 public class graphActivity extends AppCompatActivity {
 
     final String TAG = "GraphActivity";
     List<SensorData> totalSensorData = new ArrayList<>();
     List<SensorData> graphData = new ArrayList<>();
 
+    List<List<SensorData>> GraphDataYear = new ArrayList<List<SensorData>>();
+
     String sensorID;
     FirebaseAuth mAuth;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+    LineChartView graph = (LineChartView) findViewById(R.id.graph);
+    Spinner spinner = (Spinner) findViewById(R.id.yearSelector);
+    Button refreshGraphButton = (Button) findViewById(R.id.refreshGraphButton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
 
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
+        }
+
         // Enable the back button to the mainActivity
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    public static class PlaceholderFragment extends Fragment {
+
+    }
+
 
     private void getPassedData() {
         Intent received = getIntent();
